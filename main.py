78d0fea65e86b49d3449ebd800ae9febc340cd17
xuek_github <xuekai0201@163.com>
2025-04-21 20:13:41 +0800

    ubt测试：修改ssh公钥

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ import points.points_eval as pts_eval
 if __name__ == '__main__':
     folder = r'/home/xuek/桌面/TestData/input/staticmap_test/IOU'
     folder_out = r'/home/xuek/桌面/PTV3_Versions/ptv3_raser_cpu/ptv3_deploy/scripts/output/IOU/04073'
-    test_ubutnu = 2
+    test_ubutnu = 3
 
 
 
