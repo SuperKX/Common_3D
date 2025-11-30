893af94ce340e4472cbdda334b15a6740305a5bb
xuek_github <xuekai0201@163.com>
2025-11-30 20:21:55 +0800

    优化预处理功能及标签版本管理。 1、标签版本放到labeldict中，包括转换。 2、预处理可读性更好，更容易理解。 3、预处理提供三种接口：指定标签版本自适应处理，指定标签映射方式，默认标签。

--- application/PTV3_dataprocess/label_dict.py
+++ application/PTV3_dataprocess/label_dict.py
@@ -1,42 +1,181 @@
 '''
     该文档记录各版本标注数据类别，以及类别映射关系。
 '''
+class LabelRegistry:
+    """Centralized registry for label definitions and mappings"""
+    # Label definitions
+    label_def = {
+        'label12_V1': {
+            0: "background", 1: "building", 2: "wigwam", 3: "car",
+            4: "vegetation", 5: "farmland", 6: "shed", 7: "stockpiles",
+            8: "bridge", 9: "pole", 10: "others", 11: "grass"
+        },
+        'label06_V1': {
+            0: "background", 1: "building", 2: "car", 3: "vegetation",
+            4: "farmland", 5: "grass"
+        },
+        'label05_V1': {
+            0: "background", 1: "building", 2: "car", 3: "vegetation",
+            4: "grass"
+        },
+        'label04_V1': {
+            0: "background", 1: "building", 2: "vegetation", 3: "farmland"
+        }
+    }
 
-label12_V1 = {
-    0: "background",    1: "building",  2: "wigwam",    3: "car",
-    4: "vegetation",    5: "farmland",  6: "shed",      7: "stockpiles",
-    8: "bridge",        9: "pole",      10: "others",   11: "grass"
-}
+    # Label mappings between different versions
+    label_mappings = {
+        'label12_V1_to_label06_V1': {
+            0: [0, 7, 9, 10],
+            1: [1, 2, 6, 8],
+            2: [3],
+            3: [4],
+            4: [5],
+            5: [11]
+        },
+        'label12_V1_to_label05_V1': {
+            0: [0, 7, 9, 10],
+            1: [1, 2, 6, 8],
+            2: [3],
+            3: [4, 5],
+            4: [11]
+        },
+        'label12_V1_to_label04_V1': {
+            0: [0, 7, 9, 10, 3, 11],
+            1: [1, 2, 6, 8],
+            2: [4],
+            3: [5]
+        },
+        'label04_V1_to_label05_V1': {
+            0: [0],
+            1: [1],
+            2: [3],
+            3: [2]
+        }
+    }
 
-label06_V1 = {
-    0: "background",   1: "building",     2: "car",     3: "vegetation",
-    4: "farmland",     5: "grass"
-}
-label05_V1 = {
-    0: "background",   1: "building",     2: "car",     3: "vegetation",
-    4: "grass"
-}
+    @staticmethod
+    def get_labels_property_name(property_names):
+        '''
+        获取可能的标签名，即包含"class、label"的变量，且不包含坐标等名称。
+        '''
+        lable_names = set([property_name for property_name in property_names if 'label' in property_name
+                       or 'class' in property_name])
+        other_property_names = set([property_name for property_name in property_names if 'color' in property_name
+                       or 'coord' in property_name])
+        return lable_names - other_property_names
 
-label04_V1 = {
-    0: "background",   1: "building",     2: "vegetation",     3: "farmland"
-}
+    @staticmethod
+    def labels_correct_from_ply(labels_in):
+        '''
+        筛选所有标签名，并更正。
+        专门为ply文件读写使用的标签矫正，去除ply文件读入后的前缀element名，如"vertex_", "label_"
+
+        参数：
+            labels_in   输入标签，“elenmet名_label名”，如"vertex_class", "label_label12_V1"
+            return      去除前缀"vertex_", "label_"的结果
+        '''
+        corrected_labels = []
+        for label in labels_in:
+            # 去除前缀，如"label_" 或 "vertex_"
+            if label.startswith('vertex_'):
+                label = label[7:]
+            elif label.startswith('label_'):
+                label = label[6:]
+            corrected_labels.append(label)
+        if len(corrected_labels) == 0:
+            raise Exception("No 'pcl_elemnet style' label found!")
+        return corrected_labels
+
+    @staticmethod
+    def label_element_style(lable_name):
+        '''
+        label转 ply的 element风格，如{'vertex_label05_V1','label_label12_V1'}
+        '''
+        return set('vertex_' + lable_name, 'label_' + lable_name)
+
+    @classmethod
+    def label_map(cls, labels_in_ply, label_out):
+        '''
+        筛选最优标签、及对应的映射关系。
+        注意：
+            1、兼容ply的element格式的类别命名，如{'label_label05_V1','label_label12_V1'}
+        参数：
+            labels_in_ply：标签列表，如{'label05_V1','label12_V1'}  （注意：去除ply element格式的标签）
+            label_out：目标标签，如'label05_V1'
+        return
+            label_name  选择的最佳标签名
+            mapping     最佳映射关系
+        '''
+        labels_in = cls.labels_correct_from_ply(labels_in_ply)  # 去掉前缀的标签
+        if labels_in.size == 0:
+            raise Exception("labels_in is empty!")
+
+        # 1 同标签不修改
+        if label_out in labels_in:
+            return label_out, dict()  # 返回原标签
+        # 2 最优版本标签
+        available_label_name = set()
+        if 'label12_V1' in labels_in:
+            available_label_name.add('label12_V1')
+        else:
+            available_label_name = set(cls.label_def.keys()) & set(labels_in)  # 可用标签
+            if len(available_label_name) == 0:
+                raise Exception("No available label!")
+        # 3 计算标签映射
+        for label_name in available_label_name:
+            mapping_name = label_name + '_to_' + label_out
+            if mapping_name not in cls.label_mappings:
+                continue
+            else:  # label_name 映射回原始列表中
+                mapping = cls.label_mappings[mapping_name]
+                if label_name in labels_in_ply:
+                    return_name = label_name
+                else:
+                    ply_lable_name_set = cls.label_element_style(label_name)
+                    ply_lable_name_set.add(label_name)
+                    return_name = ply_lable_name_set.intersection(labels_in_ply).pop()
+                return return_name, mapping
+        raise Exception("No mapping found!")
+
+    @classmethod
+    def label_map(cls, labels_in, label_out):
+        '''
+        筛选最优标签、及对应的映射关系。
+        参数：
+            labels_in：标签列表，如{'label05_V1','label12_V1'}  （注意：去除ply element格式的标签）
+            label_out：目标标签，如'label05_V1'
+        return
+            label_name  选择的最佳标签名
+            mapping     最佳映射关系
+        '''
+        if len(labels_in) == 0:
+            raise Exception("labels_in is empty!")
+
+        # 1 同标签不修改
+        if label_out in labels_in:
+            return label_out, dict()  # 返回原标签
+        # 2 最优版本标签
+        available_label_name = set()
+        if 'label12_V1' in labels_in:
+            available_label_name.add('label12_V1')
+        else:
+            available_label_name = set(cls.label_def.keys()) & set(labels_in)  # 可用标签
+            if len(available_label_name) == 0:
+                raise Exception("No available label!")
+        # 3 计算标签映射
+        for label_name in available_label_name:
+            mapping_name = label_name + '_to_' + label_out
+            if mapping_name not in cls.label_mappings:
+                continue
+            else:
+                mapping = cls.label_mappings[mapping_name]
+                return label_name, mapping
+        raise Exception("No mapping found!")
 
 
-# 创建标签映射
-label_map_l12v1_to_l06v1 = {0: [0, 7, 9, 10],
-                             1: [1, 2, 6, 8],
-                             2: [3, ],
-                             3: [4, ],
-                             4: [5, ],
-                             5: [11, ]}
 
-label_map_l12v1_to_l05v1 = {0: [0, 7, 9, 10],
-                            1: [1, 2, 6, 8],
-                            2: [3, ],
-                            3: [4, 5],
-                            4: [11, ]}
 
-label_map_l12v1_to_l04v1 = {0: [0, 7, 9, 10, 3, 11],
-                            1: [1, 2, 6, 8],
-                            2: [4, ],
-                            3: [5, ]}
+
+
+
--- application/PTV3_dataprocess/preprocess.py
+++ application/PTV3_dataprocess/preprocess.py
@@ -50,7 +50,7 @@ def val_pth(folder_path):
 
     return
 
-def cloudinfo_to_ptv3dict(cloud_info,generate_normals=True, sample_grid=0.4):
+def cloudinfo_to_ptv3dict(cloud_info, sample_grid=0.4):
     '''
     输入一个点云信息字典，返回PTV3处理的数据字典
     （并进行将采样、计算点法向）
@@ -65,7 +65,7 @@ def cloudinfo_to_ptv3dict(cloud_info,generate_normals=True, sample_grid=0.4):
     # 1 点云grid sample
     if not 'coords' in cloud_info:
         raise ValueError(f'【错误】输入点云没有坐标信息！')
-    idx_sample = grid_sample_idx(cloud_info['coords'], grid_size=0.4)
+    idx_sample = grid_sample_idx(cloud_info['coords'], grid_size=sample_grid)
     # 2 采样后点云构造
     # 2.1 坐标
     coords_sub = cloud_info['coords'][idx_sample]
@@ -75,8 +75,8 @@ def cloudinfo_to_ptv3dict(cloud_info,generate_normals=True, sample_grid=0.4):
         colors_sub = cloud_info['colors'][idx_sample]
         save_dict['color']=colors_sub
     # 2.3 法向
-    if generate_normals == True:
-        if 'normals' in cloud_info and cloud_info['normals'] != None:
+    if 'normals' in cloud_info:
+        if cloud_info['normals'] != None:
             normals_sub = cloud_info['normals'][idx_sample]
         else:
             radius = 0.5  # 搜索半径
@@ -101,7 +101,164 @@ def cloudinfo_to_ptv3dict(cloud_info,generate_normals=True, sample_grid=0.4):
         write_ply_file(file_out, cloud_ndarray)
     return save_dict
 
+
+def get_ply_file_info(ply_file, info_needed=['coords', 'colors', 'labels', 'normals'], label_process = None):
+    '''
+    从PLY文件中提取指定的信息
+    Args:
+        ply_file (str): PLY文件路径
+        info_needed (list): 需要提取的信息列表，可包含'coords', 'colors', 'labels', 'normals'
+        label_process (str, dict or None): 标签处理方式
+            - 如果是字符串：表示目标标签名称，如'label12_V1'
+            - 如果是字典：表示外部输入标签映射关系，如 { 0: [0], 1: [1], 2: [3], 3: [2]}
+            - 如果是None：直接使用原始标签
+    Returns:
+        dict: 包含所需信息的字典，根据info_needed参数返回相应键值对
+        
+    Raises:
+        ValueError: 当必需的信息缺失时抛出异常
+    '''
+    # 点云字典
+    cloud_info = dict()
+    points_dict = points_io.parse_cloud_to_dict(ply_file)
+    # 1 坐标
+    if 'coords' in info_needed and 'coords' in points_dict:
+        coords = points_dict['coords']
+        cloud_info['coords']=coords
+    else:
+        raise ValueError(f'没有坐标信息！')
+    # 2 颜色
+    if 'colors' in info_needed and 'colors' in points_dict:
+        colors = points_dict['colors']
+        cloud_info['colors'] = colors
+    else:
+        raise ValueError(f'没有颜色信息！')
+    # 3 法向
+    if 'normals' in info_needed:
+        if 'normals' in points_dict:
+            normals = points_dict['normals']
+        else:
+            normals = None  # 此处不处理，采样后再计算。
+        cloud_info['normals'] = normals
+
+    # 4 标签
+    if 'labels' in info_needed:
+        # 4.1 指定标签名
+        if isinstance(label_process, str):
+            label_name = label_process  # 写出的标签名
+            # 0) 获取标签名列表,如{'label12_v1’,’label05_V1‘}
+            labels_list = label_dict.LabelRegistry.labels_correct_from_ply(points_dict.keys())
+            # 1) 计算最优标签映射
+            label_input_name, label_mapping = label_dict.LabelRegistry.label_map(labels_list, label_name)
+            labels = points_dict[label_input_name]
+            # 2) 进行标签映射
+            print(f'原始标签统计：')
+            points_eval.label_rate(labels)
+            new_labels = points_eval.label_change(labels, label_mapping)
+            print(f'新标签统计：')
+            points_eval.label_rate(new_labels)
+            cloud_info['labels'] = new_labels
+        # 4.2 指定标签映射
+        elif isinstance(label_process, dict):
+            # 1) 获取唯一标签名：判定为包含label、或class的字段  #  TODO： 筛选唯一标签的方法待优化
+            lable_names = label_dict.LabelRegistry.get_labels_property_name(points_dict.keys())
+            if len(lable_names) == 0:
+                raise ValueError(f'没有标签信息！')
+            elif len(lable_names) > 1:
+                raise ValueError(f'多个标签信息！')
+            label_name = lable_names.pop()
+            labels = points_dict[label_name]
+            # 2) 映射字典
+            label_mapping = label_process
+            # 3) 进行标签映射
+            print(f'原始标签统计：')
+            points_eval.label_rate(labels)
+            new_labels = points_eval.label_change(labels, label_mapping)
+            print(f'新标签统计：')
+            points_eval.label_rate(new_labels)
+            cloud_info['labels'] = new_labels
+        else:
+            # 1) 获取唯一标签名：判定为包含label、或class的字段  #  TODO： 筛选唯一标签的方法待优化
+            lable_names = label_dict.LabelRegistry.get_labels_property_name(points_dict.keys())
+            if len(lable_names) == 0:
+                raise ValueError(f'没有标签信息！')
+            elif len(lable_names) > 1:
+                raise ValueError(f'多个标签信息！')
+            label_name = lable_names.pop()
+            labels = points_dict[label_name]
+            # 3) 进行标签映射
+            print(f'不转换标签，标签统计结果：')
+            points_eval.label_rate(labels)
+            cloud_info['labels'] = labels
+
+    return cloud_info
+
+
 def preprocess_BIMTwins(
+        input_file, output_file,
+        info_needed=['coords', 'colors', 'labels', 'normals'],
+        label_process='label05_V1',
+        sample_grid=0.4, val_result_pth=True
+):
+
+    # 1 获取需要的点云信息
+    cloud_info = get_ply_file_info(input_file, info_needed, label_process)
+
+    # 2 将采样、点云化，处理为ptv3输入格式
+    # （注意：当前不再构造kdtree上采样树）
+    save_dict = cloudinfo_to_ptv3dict(cloud_info, sample_grid=sample_grid)
+    if True:  # train模式需要
+        scene_id = os.path.splitext(os.path.split(input_file)[1])[0]
+        save_dict['scene_id'] = scene_id
+    # 写出采样后的点云的 pth文件
+    torch.save(save_dict, output_file)
+
+    if val_result_pth:
+        print(f"验证pth数据标签（降采样结果）：", end="")
+        val_pth(output_file)
+    return
+
+def preprocess_BIMTwins_batch(
+        dataset_root, output_root,
+        info_needed=['coords', 'colors', 'labels', 'normals'],
+        label_process='label05_V1',
+        sample_grid=0.4, val_result_pth=True
+):
+    # 1 数据地址
+    # 查找所有ply文件
+    sceneList = sorted(glob.glob(dataset_root + "/train/*.ply"))
+    sceneList += sorted(glob.glob(dataset_root + "/val/*.ply"))
+    sceneList += sorted(glob.glob(dataset_root + "/test/*.ply"))
+
+    # 2 创建输出文件地址
+    train_output_dir = os.path.join(output_root, "train")
+    os.makedirs(train_output_dir, exist_ok=True)
+    val_output_dir = os.path.join(output_root, "val")
+    os.makedirs(val_output_dir, exist_ok=True)
+    test_output_dir = os.path.join(output_root, "test")
+    os.makedirs(test_output_dir, exist_ok=True)
+
+    # 3 批量处理
+    time_start0 = time.time()
+    for input_file in sceneList:
+        time_starti = time.time()
+        subFoler = os.path.split(os.path.split(input_file)[0])[1]  # 'train'
+        scene_id = os.path.splitext(os.path.split(input_file)[1])[0]  # 'JXY'
+        output_file = os.path.join(output_root, subFoler, f"{scene_id}.pth")
+        print(f"Processing: {scene_id} in {subFoler}")
+
+        preprocess_BIMTwins(
+            input_file, output_file,
+            info_needed=info_needed,
+            label_process=label_process,
+            sample_grid=sample_grid, val_result_pth=val_result_pth
+        )
+
+        print(f"场景 {scene_id} 耗时：{time.time() - time_starti}")
+        print(" ")
+    print(f"预处理 总耗时：{time.time() - time_start0}")
+
+def preprocess_BIMTwins_备份1130(
         input_file, output_file,
         generate_normals=True, sample_grid=0.4,
         label_dict={},
@@ -159,69 +316,11 @@ def preprocess_BIMTwins(
     torch.save(save_dict, output_file)
 
     if val_result_pth:
-        print(f"验证pth数据标签（降采样结果）：",end="")
+        print(f"验证pth数据标签（降采样结果）：", end="")
         val_pth(output_file)
     return
 
-def preprocess_BIMTwins_备份(
-        input_file, output_file,
-        generate_normals=True, sample_grid=0.4,
-        label_dict={},
-        val_result_pth=True,
-        create_fake_label=False,
-):
-    '''
-    brief:
-        预处理：读入原始1个点云数据，生成ptv3训练输入的降采样点云.pth
-        注意：
-            1）输入是点云地址，写出pth文件地址
-            2）label_dict要求如下描述。
-    paras:
-        input_file          输入原始点云
-        output_file         写出pth文件
-        generate_normals    是否写出法向
-        sample_grid         采样密度
-        label_dict          新旧字典映射：{新标签1：[旧标签1，就标签2, ...], },ex: label_dict={0:[0,1],1:[2,3]}；
-                                1）未修改标签则置为0，会提示。
-                                2）空，表示不修改标签，会提示。
-        val_result_pth      是否验证生成的pth文件
-        create_fake_label   如果没有标签，则创造假标签
-    '''
-    # 1 获取点云信息：坐标、颜色、标签
-    try:
-        coords, colors, labels = points_io.parse_ply_file(input_file)
-    except ValueError as e:
-        raise ValueError(f'发生错误{e}')
-    if not isinstance(coords, np.ndarray):
-        raise ValueError(f'没有点')
-    elif not isinstance(labels, np.ndarray):
-        if not create_fake_label:
-            raise ValueError(f'输入数据没有标签！（如需构造虚假标签，修改变量“create_fake_label”）')
-        else:
-            print(f"【警告】未找到标签信息，此处将构造虚假标签！")
-            labels = np.zeros(len(coords), int)
-
-    # 3 标签替换
-    new_labels = points_eval.label_change(labels, label_dict)
-
-    # 4 构造cloud_info点云信息
-    cloud_info = dict(coords=coords, colors=colors, labels=new_labels)
-
-    # 5 将采样、点云化，处理为ptv3输入格式
-    # （注意：当前不再构造kdtree上采样树）
-    save_dict = cloudinfo_to_ptv3dict(cloud_info, generate_normals=generate_normals, sample_grid=sample_grid)
-    if True:  # train模式需要
-        scene_id = os.path.splitext(os.path.split(input_file)[1])[0]
-        save_dict['scene_id'] = scene_id
-    # 写出采样后的点云的 pth文件
-    torch.save(save_dict, output_file)
-
-    if val_result_pth:
-        print(f"验证pth数据标签（降采样结果）：",end="")
-        val_pth(output_file)
-    return
-
-def preprocess_BIMTwins_batch(
+def preprocess_BIMTwins_batch_备份1130(
         dataset_root, output_root,
         generate_normals=True, sample_grid=0.4,
         label_dict={},
@@ -269,8 +368,34 @@ def preprocess_BIMTwins_batch(
 '''
 if __name__ == "__main__":
 
-    # 预处理数据
-    if True:  # 单个数据
+    if True:  # 批量处理
+        dataset_root = r'/media/xuek/Data210/数据集/临时测试区/训练集_temp'
+        output_root = r'/media/xuek/Data210/数据集/临时测试区/训练集_temp_out'
+        info_needed = ['coords', 'colors', 'labels', 'normals']
+        '''
+        label_process 调用说明：
+            方法1： label_process = 'label05_V1'  
+                直接说明调用的标签版本，前提是数据集均为统一命名方式。
+                找不到则会选择最佳转换方式。
+            方法2： label_process = label_dict.LabelRegistry.label_mappings['label12_V1_to_label05_V1'] 
+                直接给出映射字典，会调用唯一标签执行映射。
+                存在多标签会报错
+            方法3： label_process = None 不修改标签，直接使用默认标签执行。
+        '''
+        # label_process = 'label05_V1'
+        label_process = label_dict.LabelRegistry.label_mappings['label12_V1_to_label05_V1']
+        # label_process = None
+        sample_grid = 0.4  # 采样大小：注意此处写死
+        preprocess_BIMTwins_batch(
+            dataset_root, output_root,
+            info_needed=info_needed,
+            label_process=label_process,
+            sample_grid=sample_grid, val_result_pth=True
+        )
+
+
+    # 预处理数据  # 弃用
+    if False:  # 单个数据
         time_starti = time.time()
         input_file =r'/media/xuek/Data210/数据集/训练集/重建数据_版本2025.10.15/train/06XCNC.ply'
         output_file = r'/home/xuek/桌面/TestData/临时测试区/重建数据_版本2025.10.15_weight20251113/test.pth'
@@ -284,12 +409,12 @@ if __name__ == "__main__":
             create_fake_label=False,
         )
         print(f"场景  耗时：{time.time() - time_starti}")
-    if False:  # 批量处理
+    if False:  # 批量处理（老版本）  # 弃用
         # 0 参数修改
-        dataset_root = r'/media/xuek/Data210/数据集/训练集/重建数据_版本2025.10.15'
-        output_root = r'/home/xuek/桌面/TestData/PTV3_data/重建数据_版本2025.10.15_5class_1113'
+        dataset_root = r'/media/xuek/备份盘/TempProcess/20251125数据集/LABEL12'
+        output_root = r'/home/xuek/桌面/TestData/PTV3_data/重建数据_版本2025.10.15_5class_1125'
         sample_grid = 0.4  # 采样大小：注意此处写死
-        label_dict_input = label_dict.label_map_l12v1_to_l05v1
+        label_dict_input = label_dict.label_map_l04v1_to_l05v1
         preprocess_BIMTwins_batch(
                 dataset_root, output_root,
                 generate_normals=True, sample_grid=0.4,
@@ -297,3 +422,4 @@ if __name__ == "__main__":
                 val_result_pth=True,
                 create_fake_label=False,
         )
+
